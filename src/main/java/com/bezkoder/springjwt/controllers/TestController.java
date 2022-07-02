0a6cd7df42c11562e2597631833c0c3096ef3e30
Nam <namandroid.it@gmail.com>
2022-07-02 20:52:47 +0700

    co thay doi

---
+++ src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -0,0 +1,41 @@
+package com.bezkoder.springjwt.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+@Slf4j
+public class TestController {
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public String userAccess() {
+		return "User Content.";
+	}
+
+	@GetMapping("/mod")
+	@PreAuthorize("hasRole('MODERATOR')")
+	public String moderatorAccess() {
+		return "Moderator Board.";
+	}
+
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+
+	@PostMapping("/data")
+	public ResponseEntity<?> getDatatest(){
+		log.info("co gui gui den");
+		return ResponseEntity.ok().body("pham van nam");
+	}
+}
